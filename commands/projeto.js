cadfe17deee7ec895bf90e77b510fc217d2547ca
MarusDod <markitos.2001@hotmail.com>
2022-07-18 02:27:01 +0100

    .

--- commands/projeto.js
+++ commands/projeto.js
@@ -10,12 +10,18 @@ module.exports = {
         const oneDay = 24*60*60 * 1000; // hours*minutes*seconds*milliseconds
         const oneSecond = 1000; // hours*minutes*seconds*milliseconds
         const currDate = new Date()
+	const hours = Math.floor(diffSeconds / 3600)
+	const delta = hours * 3600
+	
+	const minutes = Math.floor((diffSeconds / 60) % 60)
+	delta += minutes * 60
+	const seconds = delta % 60
 
         //const diffDays = Math.ceil(Math.abs((entregaDate.getTime() - currDate.getTime()) / oneDay));
 	const diffSeconds = Math.ceil(Math.abs((entregaDate.getTime() - currDate.getTime()) / oneSecond));
 
         //message.channel.send('o que interessa é ter saúde :)')
         //message.channel.send(`faltam ${diffDays} dias para a entrega do projeto`)
-        message.channel.send(`faltam ${Math.floor(diffSeconds / 3600)} horas ${Math.floor((diffSeconds / 60) % 60)} minutos e ${Math.floor(diffSeconds)} segundos para entregar o projeto ou fudeu`)
+        message.channel.send(`faltam ${hours} horas ${minutes} minutos e ${seconds} segundos para entregar o projeto ou fudeu`)
     },
 };
